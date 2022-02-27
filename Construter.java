27818515bf5dba84d436c081f20a3ad63b2a3fcc
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2022-02-27 22:28:03 +0530

    update

---
+++ Construter.java
@@ -0,0 +1,36 @@
+class Employee
+{
+	int age;
+	String name;
+	
+	//Default Constructor
+	Employee()
+	{
+		age =100;
+		name="Test1";
+	}
+	
+	//Parameterized Constructor
+	Employee(int age,String name)
+	{
+		this.age =age;
+		this.name=name;
+	}
+	
+	public void disp()
+	{
+		System.out.println("Name : "+name+" Age : "+age);
+	}
+	
+}
+public class ConstructorOverloadingExample 
+{
+	public static void main(String args[])
+	{
+		Employee e1 = new Employee();
+		Employee e2 = new Employee(10,"Test2");
+		
+		e1.disp();
+		e2.disp();
+	}
+}
