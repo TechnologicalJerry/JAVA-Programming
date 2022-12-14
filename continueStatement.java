d6988dfa468469b13bb9711f548fec410f6fd4e7
technologicaljerry <cserajatverma@gmail.com>
2022-12-14 23:49:41 +0530

    Java Continue Statement Program

---
+++ continueStatement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Double number, sum = 0.0;
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i < 6; ++i) {
+            System.out.print("Enter number " + i + " : ");
+
+            number = input.nextDouble();
+
+            if (number <= 0.0) {
+                continue;
+            }
+
+            sum += number;
+        }
+        System.out.println("Sum = " + sum);
+        input.close();
+    }
+}
