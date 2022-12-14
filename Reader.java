22bd2ea825a1e68d13ffbd1d121e87ea1b5f23f5
technologicaljerry <cserajatverma@gmail.com>
2022-12-14 23:59:07 +0530

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,21 +1,30 @@
-public
+import java.io.Reader;
+import java.io.FileReader;
 
-import java.io.*;
+class Main {
+    public static void main(String[] args) {
 
-public class Reader {  
-    public static void main(String[] args) {  
-        try {  
-            Reader reader = new FileReader("file.txt");  
-            int data = reader.read();  
-            while (data != -1) {  
-                System.out.print((char) data);  
-                data = reader.read();  
-            }  
-            reader.close();  
-        } catch (Exception ex) {  
-            System.out.println(ex.getMessage());  
-        }  
-    }  
-}Reader{
+        // Creates an array of character
+        char[] array = new char[100];
 
+        try {
+            // Creates a reader using the FileReader
+            Reader input = new FileReader("input.txt");
+
+            // Checks if reader is ready
+            System.out.println("Is there data in the stream?  " + input.ready());
+
+            // Reads characters
+            input.read(array);
+            System.out.println("Data in the stream:");
+            System.out.println(array);
+
+            // Closes the reader
+            input.close();
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
 }
