6d1cb13afad1672098d6bdf391ee9f9e0b1c3d0a
technologicaljerry <cserajatverma@gmail.com>
2023-02-02 22:53:18 +0530

    Update devision.java

--- devision.java
+++ devision.java
@@ -1,10 +1,7 @@
 public class devision {
 
+    public static void main(String[] args) {
+        System.out.println(50 / 10);
+    }
 
- 
-        public static void main(String[] args) {
-         System.out.println(50/3);
-        }
-        
-    
 }
