debecb98360b515473d51264744333a5ba3d856d
technologicaljerry <cserajatverma@gmail.com>
2023-02-02 22:51:34 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,13 +1,7 @@
 public class addition {
 
+    public static void main(String[] args) {
+        System.out.println(240 + 260);
+    }
 
-
-
- 
-        public static void main(String[] args) {
-         System.out.println(24+26);
-        }
-        
-
-    
 }
